309969c7dd0c8804577338cf9b45e3e8318aaaa3
urnaa <uranchimeg1201@gmail.com>
2020-04-13 04:32:16 +0800

    shape, play, number huudasnudig shineer vvsgew

--- navigation/homeStack.js
+++ navigation/homeStack.js
@@ -3,18 +3,20 @@ import Home from "../screens/home";
 import Animal from "../screens/animal";
 import Color from "../screens/color";
 import Flag from "../screens/flag";
+import Shape from "../screens/shape";
+import Number from "../screens/number";
+import Play from "../screens/play";
 import Header from "../components/header";
-import React from 'react';
+import React from "react";
 const screens = {
-  Home: {   
+  Home: {
     screen: Home,
-    navigationOptions: ({navigation}) => {
-        return {
-          headerTitle : () => <Header navigation = {navigation}/>,
-        }
-      }
+    navigationOptions: ({ navigation }) => {
+      return {
+        headerTitle: () => <Header navigation={navigation} />,
+      };
     },
-  
+  },
 
   Animal: {
     screen: Animal,
@@ -23,15 +25,25 @@ const screens = {
   Color: {
     screen: Color,
   },
-  Flag :{
-      screen: Flag,
+  Flag: {
+    screen: Flag,
+  },
+  Number:{
+    screen: Number,
+  },
+  Shape: {
+    screen: Shape,
+  },
+  Play: {
+    screen: Play,
   }
+ 
+ 
 };
 
 const HomeStack = createStackNavigator(screens, {
   defaultNavigationOptions: {
-
-    headerStyle: { backgroundColor: "rgb(200,255, 255)",  height: 60},
+    headerStyle: { backgroundColor: "rgb(200,255, 255)", height: 60 },
   },
 });
 
